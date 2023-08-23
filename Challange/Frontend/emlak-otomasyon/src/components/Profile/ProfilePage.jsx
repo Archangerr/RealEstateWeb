254344bb7b2509755e1fd06b09998a5fe26bed97
Tunç Poyraz <osmantuncpoyraz@gmail.com>
2023-08-23 16:37:02 +0300

    isAvailable ile beraber gorunurluk eklendi

--- Challange/Frontend/emlak-otomasyon/src/components/Profile/ProfilePage.jsx
+++ Challange/Frontend/emlak-otomasyon/src/components/Profile/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams,Navigate } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import './ProfilePage.css';
 import { fetchProfile } from '../../services/ProfileService';
 import Map from '../Map/Map';
@@ -19,6 +19,7 @@ function ProfilePage() {
         try {
             const data = await fetchProfile(emlakciId);
             setEmlakci(data);
+            console.log("data", data);
 
         } catch (error) {
             console.error("Error fetching Emlak data:", error);
@@ -56,51 +57,97 @@ function ProfilePage() {
                             <p><strong>Email:</strong> {emlakci.email}</p>
                         </div>
                         <h3>Emlak List</h3>
-
-                        {emlakci.emlakDetails.map((emlak, index) => (
-
-                            <div className="card">
-                                <h5 className="card-header">{emlak.title}</h5>
-                                <div className="card-body  mb-1">
-                                    <div className="row">
-                                        <div className="col-md-4">
-                                            {emlak.imageBases.map((imgSrc, imgIndex) => (
-                                                <img
-                                                    key={imgIndex}
-                                                    src={imgSrc}
-                                                    alt='Img'
-                                                    className={` ${imgIndex === 0 ? 'img-thumbnail' : 'second-img'}`}
+                        <div>
+                            <h3>Available Emlak List</h3>
+                            {emlakci.emlakDetails.filter(e => e.isAvailable).map((emlak, index) => (
+                                <div className="card">
+                                    <h5 className="card-header">{emlak.title}</h5>
+                                    <div className="card-body  mb-1">
+                                        <div className="row">
+                                            <div className="col-md-4">
+                                                {emlak.imageBases.map((imgSrc, imgIndex) => (
+                                                    <img
+                                                        key={imgIndex}
+                                                        src={imgSrc}
+                                                        alt='Img'
+                                                        className={` ${imgIndex === 0 ? 'img-thumbnail' : 'second-img'}`}
+                                                    />
+                                                ))}
+                                            </div>
+                                            <div className="col-md-2" >
+                                                <p> <strong>Durumu :</strong> {emlak.durumu}</p>
+                                                <p> <strong>Type :</strong> {emlak.type}</p>
+                                                <p> <strong>Fiyati :</strong> {emlak.fiyat} {emlak.doviz}</p>
+                                                <p> <strong>Kalan Sure :</strong> {handleRemainingTime(emlak)} saat</p>
+                                            </div>
+                                            <div className="col-md-5">
+                                                <Map
+                                                    latitude={emlak.latitude}
+                                                    longitude={emlak.longitude}
+                                                    width='100%'
+                                                    height='300px'
                                                 />
-                                            ))}
-                                        </div>
-                                        <div className="col-md-2" >
-                                            <p> <strong>Durumu :</strong> {emlak.durumu}</p>
-                                            <p> <strong>Type :</strong> {emlak.type}</p>
-                                            <p> <strong>Fiyati :</strong> {emlak.fiyat} {emlak.doviz}</p>
-                                            <p> <strong>Kalan Sure :</strong> {handleRemainingTime(emlak)} saat</p>
-                                        </div>
-                                        <div className="col-md-5">
-                                            <Map
-                                                latitude={emlak.latitude}
-                                                longitude={emlak.longitude}
-                                                width='100%'
-                                                height='300px'
-                                            />
-                                        </div>
-                                        <div className="col-md-1">
-                                            <Link to={`/EmlakEdit/${emlak.id}`}>
-                                                Edit
-                                            </Link>
-                                            <button>
+                                            </div>
+                                            <div className="col-md-1">
+                                                <Link to={`/EmlakEdit/${emlak.id}`}>
+                                                    Edit
+                                                </Link>
+                                                <button>
 
-                                            </button>
+                                                </button>
 
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            ))}
+                        </div>
+                        <div>
+                            <h3>Unavailable Emlak List</h3>
+                            {emlakci.emlakDetails.filter(e => !e.isAvailable).map((emlak, index) => (
 
-                        ))}
+                                <div className="card">
+                                    <h5 className="card-header">{emlak.title}</h5>
+                                    <div className="card-body  mb-1">
+                                        <div className="row">
+                                            <div className="col-md-4">
+                                                {emlak.imageBases.map((imgSrc, imgIndex) => (
+                                                    <img
+                                                        key={imgIndex}
+                                                        src={imgSrc}
+                                                        alt='Img'
+                                                        className={` ${imgIndex === 0 ? 'img-thumbnail' : 'second-img'}`}
+                                                    />
+                                                ))}
+                                            </div>
+                                            <div className="col-md-2" >
+                                                <p> <strong>Durumu :</strong> {emlak.durumu}</p>
+                                                <p> <strong>Type :</strong> {emlak.type}</p>
+                                                <p> <strong>Fiyati :</strong> {emlak.fiyat} {emlak.doviz}</p>
+                                                <p> <strong>Kalan Sure :</strong> {handleRemainingTime(emlak)} saat</p>
+                                            </div>
+                                            <div className="col-md-5">
+                                                <Map
+                                                    latitude={emlak.latitude}
+                                                    longitude={emlak.longitude}
+                                                    width='100%'
+                                                    height='300px'
+                                                />
+                                            </div>
+                                            <div className="col-md-1">
+                                                <Link to={`/EmlakEdit/${emlak.id}`}>
+                                                    Edit
+                                                </Link>
+                                                <button>
+
+                                                </button>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
 
                     </div>
                 )
